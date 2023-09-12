b1d858d199f97f9d37845ed80cf32e77a2bf22be
noahirg <noahirgang@gmail.com>
2023-09-11 20:37:56 -0400

    Show found words function

--- SpellingBee/Game.cs
+++ SpellingBee/Game.cs
@@ -6,5 +6,21 @@ namespace SpellingBee
 {
     internal class Game
     {
+        /// <summary>
+        /// Prints out the words found by the user, in one line
+        /// </summary>
+        /// <param name="foundWords"></param>
+        public static void ShowFoundWords(List<string> foundWords)
+        {
+            Console.WriteLine("The words you have found are:");
+            foreach (string word in foundWords)
+            {
+                if (foundWords.IndexOf(word) < foundWords.Count - 1)
+                    Console.Write(word + ", ");
+
+                else
+                    Console.WriteLine(word);
+            }
+        }
     }
 }
