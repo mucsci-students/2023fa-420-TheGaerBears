a9cac5ba36bca37f68b91db5eecb7eca5e912468
noahirg <noahirgang@gmail.com>
2023-09-13 11:41:29 -0400

    Main game loop

--- SpellingBee/Program.cs
+++ SpellingBee/Program.cs
@@ -1,6 +1,8 @@
 ﻿using SpellingBee.SetUpSpellingBee.src.main;
 using System;
 using SQLitePCL;
+using SpellingBee;
+
 internal class Program
 {
     static void Main(string[] args)
@@ -8,9 +10,88 @@ internal class Program
         // Initialize SQLitePCL
         Batteries.Init();
 
-        Console.WriteLine("Start here!");
+        Console.WriteLine("Welcome to the Spelling Bee Puzzle Game!");
+        Console.WriteLine("Here are some commands to help you out:");
+        //Call help command here
+
+
         CreateDatabase db = new CreateDatabase();
-        //db.AddPangramTable("pangrams");
-        db.PrintCount("pangrams");
+
+        Game mainGame = new Game();
+
+        //While loop that keeps game running
+        while (true)
+        {
+            string input = Console.ReadLine().Trim();
+            CommandValidation(input, ref mainGame);
+        }
+    }
+
+    /// <summary>
+    /// Parses the commands entered by the player
+    /// </summary>
+    /// <param name="input"></param> The command typed by the player
+    /// <param name="mainGame"></param> The Game object for the puzzle
+    static void CommandValidation(string input, ref Game mainGame)
+    {
+        switch (input)
+        {
+            case "-help":
+                //Add when done
+                break;
+
+            case "-exit":
+                mainGame.Exit();
+                break;
+
+            case "-save current":
+                //Add when done
+                break;
+
+            case "-save puzzle":
+                //Add when done
+                break;
+
+            case "-load puzzle":
+                //Add when done
+                break;
+
+            case "-show puzzle":
+                //Needs to check if puzzle started for many of these commands
+                //Will add function to game class to check this
+                mainGame.ShowPuzzle();
+                break;
+
+            case "-show found words":
+                mainGame.ShowFoundWords();
+                break;
+
+            case "-shuffle":
+                mainGame.Shuffle();
+                break;
+
+            case "-show status":
+                mainGame.ShowStatus();
+                break;
+
+            case "-guess":
+                mainGame.Guess();
+                break;
+
+            case "-new puzzle":
+                mainGame.NewPuzzle();
+                break;
+
+            case "-new puzzle from word":
+                Console.WriteLine("Please enter a valid pangram: ");
+
+                string pang = Console.ReadLine().Trim();
+                mainGame.NewPuzzleBaseWord(pang);
+                break;
+
+            default:
+                Console.WriteLine("Sorry this is not a valid input. Please refer to '-help' for valid commands.");
+                break;
+        }
     }
 }
